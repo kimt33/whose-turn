58d98b21e15565d89fd00c4b7e5ae11fd033f7c5
Taewon Kim <kimt33@mcmaster.ca>
2015-09-09 12:01:45 -0400

    -2015.8.7 commit

---
+++ groupmeeting/2015-08-17.py
@@ -0,0 +1,11 @@
+from group_meeting import *
+from datetime import date
+import pickle
+
+groupmeetings = pickle.load(open('group_meeting.p','r'))
+groupmeetings.update_future()
+thisweek = date(2015,8,19)
+for person in groupmeetings._presenters:
+    print person.__dict__.keys()
+groupmeetings.print_nice()
+groupmeetings.compose_emails()
